dfdcdb0beea89fbf1dcd50c94f8369c32fd3daa6
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-17 00:12:54 +0600

    Set functon with output time in setTimeout

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ function timeFunc(value) {
 
 console.log(1100);
 setTimeout (timeFunc, 2000);
+
+setTimeout(function(){
+    console.log("This result showing after 3 second later")
+}, 3000);
 console.log(1200);
 console.log(1300);
